21042fc0265676c931be8a86e6c528387f8d1ff4
Alexey Golomedov <golomedov@gmail.com>
2015-11-30 16:47:32 +0600

    Sample text

--- pythonskiy.py
+++ pythonskiy.py
@@ -11,7 +11,7 @@ import pickle
 class Pythonsky(object):
     PARAGRAPH_DELIMITER = "\n\n"
     SPEACH_DELIMITER = "\"'"
-    SENTENCE_END = "?!.'"
+    SENTENCE_END = "?!."
 
     def __init__(self, work_dir):
         self._text = ""
